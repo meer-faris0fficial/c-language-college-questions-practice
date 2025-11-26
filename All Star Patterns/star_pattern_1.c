e84191a6e9ad87b73d7c19f0f7deae1f0a1c8803
Meer_Faris <mthofficial111@gmail.com>
2025-11-26 22:21:24 +0500

    Add files via upload

---
+++ All Star Patterns/star_pattern_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows = 3;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    rows = rows - 1;
+    for (int i = rows; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
